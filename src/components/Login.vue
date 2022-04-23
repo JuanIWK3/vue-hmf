de81490a87bd6e058d06acd0e9adeee6f8b1a92b
juaniwk3 <juaniwk3@gmail.com>
2022-04-23 19:30:46 -0300

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({ name: "login" });
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
