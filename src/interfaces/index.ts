0c3355efd14eb7363b4106f2cf4bb86eeaa5093f
juaniwk3 <juaniwk3@gmail.com>
2022-04-25 00:43:29 -0300

    feat(Movie): basic structure

---
+++ src/interfaces/index.ts
@@ -0,0 +1,12 @@
+interface IMovie {
+  id: string;
+  title: string;
+  description: string;
+  releaseDate: Date;
+  magnet: string;
+  image: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export default IMovie;
--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -1,16 +1,21 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import api from '../api';
+import IMovie from '../interfaces';
 export default defineComponent({
   name: 'movie',
   data() {
-    const moviesList: any = [];
-    return { moviesList };
+    const movie: IMovie | null = null;
+    const imageURL =
+      'https://homemoviefestivalbucket.s3.us-east-2.amazonaws.com/';
+    const loading = true;
+    return { movie, loading, imageURL };
   },
   methods: {
     async getMovies() {
       const res = await api.get('/movies');
-      this.moviesList = res.data;
+      this.loading = false;
+      this.movie = res.data[parseInt(this.$route.params.id as string)];
     },
   },
   mounted() {
@@ -20,9 +25,42 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>
-    {{ $route.path }}
+  <div class="container">
+    <div class="movie" v-if="movie && !loading">
+      <div class="title">{{ movie.title }}</div>
+      <img :src="imageURL + movie.image" alt="movie thumb" />
+      <div class="description">{{ movie.description }}</div>
+    </div>
+    <div v-if="loading">loading</div>
+    <div v-if="!loading && !movie">Erro ao carregar filme</div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  margin-top: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .movie {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 80vw;
+    max-width: 900px;
+    text-align: center;
+
+    .title {
+      font-size: 24px;
+      font-weight: 700;
+      margin-bottom: 16px;
+    }
+    .description {
+      font-size: 16px;
+      margin-top: 16px;
+    }
+  }
+}
+</style>
